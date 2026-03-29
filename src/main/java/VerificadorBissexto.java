b59fff99e54646b83ae9cb4a008f58144cfc1602
Pedro Augusto <pedrodias08890@gmail.com>
2026-03-28 22:32:59 -0300

    Exercício 2 módulo 3

--- src/main/java/VerificadorBissexto.java
+++ src/main/java/VerificadorBissexto.java
@@ -4,21 +4,24 @@ public class VerificadorBissexto {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Digite um ano (exemplo: 2024): ");
+        System.out.print("Digite um ano (ex: 2024): ");
         int ano = sc.nextInt();
 
-        if (ano % 400 == 0) {
-            System.out.println(ano + " é bissexto.");
-        }
-        else if (ano % 100 == 0) {
-            System.out.println(ano + " não é bissexto.");
-        }
-        else if (ano % 4 == 0) {
-            System.out.println(ano + " é bissexto.");
-        }
-        else {
-            System.out.println(ano + " não é bissexto.");
-        }
+        boolean ehBissexto;
 
+        if (ano % 400 == 0) {
+            ehBissexto = true;
+        } else if (ano % 100 == 0) {
+            ehBissexto = false;
+        } else if (ano % 4 == 0) {
+            ehBissexto = true;
+        } else {
+            ehBissexto = false;
+        }
+        if (ehBissexto) {
+            System.out.println("O ano " + ano + " é bissexto.");
+        } else {
+            System.out.println("O ano " + ano + " não é bissexto.");
+        }
     }
 }
